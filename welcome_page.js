7fdec4e0cd9067af46a50e77805130efa4f369ef
Giulio-Marra <giuliomarramail@gmail.com>
2024-03-22 12:02:08 +0100

    aa

--- welcome_page.js
+++ welcome_page.js
@@ -2,21 +2,19 @@
 
 const btn = document.getElementById("button");
 const check = document.getElementById("tmp-28");
-const checkboxDiv = document.getElementById("checkboxDiv")
+const checkboxDiv = document.getElementById("checkboxDiv");
 
-check.addEventListener (
-  "click", () => {
-    if (check.checked) {
-      btn.classList.add("glow")
-      btn.style.backgroundColor = "#00ffff"  
-      btn.style.color = "rgb(3, 3, 34)"
-    }
-    else {
-      btn.classList.remove("glow")
-      btn.style.backgroundColor = "rgb(145, 145, 145)"
-      btn.style.color= "rgb(66, 66, 66)"
-    }
-  });
+check.addEventListener("click", () => {
+  if (check.checked) {
+    btn.classList.add("glow");
+    btn.style.backgroundColor = "#00ffff";
+    btn.style.color = "rgb(3, 3, 34)";
+  } else {
+    btn.classList.remove("glow");
+    btn.style.backgroundColor = "rgb(145, 145, 145)";
+    btn.style.color = "rgb(66, 66, 66)";
+  }
+});
 /*const warning = document.getElementById("alert")
 btn.addEventListener("click", () => {
  if (!warning) {
@@ -32,13 +30,13 @@ btn.addEventListener("click", () => {
  }
 });*/
 
-const warning = document.getElementById("checked")
+const warning = document.getElementById("checked");
 btn.addEventListener("click", () => {
   if (!check.checked) {
-    warning.innerText = "PLEASE, CHECK THE BOX!"
-  }
-  else {
-    warning.innerText = ""
+    warning.innerText = "PLEASE, CHECK THE BOX!";
+  } else {
+    warning.innerText = "";
     btn.style.backgroundColor = "#9ffcfc";
     window.location.href = "quiz.html";
-  }});
+  }
+});
