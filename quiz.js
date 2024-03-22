f48130895765f4a4214a0e97883c41197d1b3acc
Giulio-Marra <giuliomarramail@gmail.com>
2024-03-22 15:19:26 +0100

    changeDate

--- quiz.js
+++ quiz.js
@@ -337,33 +337,26 @@ const createRecap = () => {
 // import correct_answer from 'quiz.js'
 // import totalScore from 'quiz.js'
 
-let params = new URLSearchParams(window.location.search); // recupero parametri passati all url baretto
-let risposteCorrette = parseInt(params.get("a")); // Recupero risposte corrette parametro URL e converto in numero intero baretto
-console.log(risposteCorrette);
-
-let totaledomande = parseInt(params.get("b")); // Recupero domande totali parametro URL e converto in numero intero baretto
-
-let rispostsbagliate = totaledomande - risposteCorrette; // Calcolo delle risposte sbagliate baretto
+correctAnswers = [];
+wrongAnswers = [];
+// amountNum = [] ho sopra il valore delle domande selezionate
 
 let scorepositivo = document.querySelector("#positiveScore");
 let scorenegativo = document.querySelector("#negativeScore");
 
-function calcolaPercentuale(totaledomande, rispostsbagliate) {
-  return (rispostsbagliate / totaledomande) * 100;
+function calcolaPercentuale(amountNum, wrongAnswers) {
+  return (wrongAnswers / amountNum) * 100;
 }
 
-let percentuale = Math.round(
-  calcolaPercentuale(totaledomande, rispostsbagliate)
-);
+let percentuale = Math.round(calcolaPercentuale(amountNum, wrongAnswers));
+
 let questions = document.getElementsByClassName("fontFix");
 for (let i = 0; i < questions.length; i++) {
-  questions[i].innerHTML =
-    risposteCorrette + "/" + totaledomande + " questions";
+  questions[i].innerHTML = correctAnswers + "/" + amountNum + " questions";
 }
 let questionWrong = document.getElementsByClassName("fontFix2");
 for (let i = 0; i < questions.length; i++) {
-  questionWrong[i].innerHTML =
-    rispostsbagliate + "/" + totaledomande + " questions";
+  questionWrong[i].innerHTML = wrongAnswers + "/" + amountNum + " questions";
 }
 questions.innerHTML;
 scorenegativo.innerHTML = percentuale + "%";
@@ -385,7 +378,7 @@ if (percentuale <= 40) {
 }
 // commento
 const labels1 = ["right", "wrong"];
-let data1 = [risposteCorrette, rispostsbagliate];
+let data1 = [correctAnswers, wrongAnswers];
 const colors1 = ["#00FFFF", "#D20094"];
 
 const myChart = document.querySelector(".my-chart");
